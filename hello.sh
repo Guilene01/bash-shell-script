31646d3c3a9945ed0b94fbd714f4e3af359b0ecb
Guilene01 <guileneerna@gmail.com>
2024-11-12 19:26:23 -0500

    first commit

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+    #Author : Utrains
+    #Date : 01-Nov-2021
+    #Modified by Guilene, nov 2024
+
+## ---------- script that display hello word in the screen -----------------
+
+echo "Hello Word !"
